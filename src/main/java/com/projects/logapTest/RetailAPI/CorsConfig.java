1037b5d7d732bb52ce1f2c666d14d5cbbcdd82da
Caio Farias <caio4095@gmail.com>
2022-07-12 18:57:59 -0300

    cors allow all origins

--- src/main/java/com/projects/logapTest/RetailAPI/CorsConfig.java
+++ src/main/java/com/projects/logapTest/RetailAPI/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
 
-    registry.addMapping("/**").allowedMethods(
+    registry.addMapping("/**").allowedOrigins("*").allowedMethods(
       "POST",
       "PATCH",
       "PUT",
